ad14224add58ec3178de4e9ffb1e3f6742717536
Mantu29 <man2g29@gmail.com>
2024-07-02 21:47:43 +0530

    2nd Week Codes

---
+++ Class/src/main/java/Week_02/FJSP.java
@@ -0,0 +1,15 @@
+package Basics.FJSP;
+
+public class FJSP {
+    public static void main(String[] args) {
+        int n = 3; //no. of job
+        int m = 3; //no. of machines
+        int o = 5; // no. of operations
+
+        List<List<Integer>> jobs = new ArrayList<List<Integer>>(); // jobs definations
+
+        
+
+
+    }
+}
---
+++ Class/src/main/java/Week_02/FJSP_IP.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class FJSP_IP {
+}
